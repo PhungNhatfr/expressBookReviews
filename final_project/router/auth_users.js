c62d46e710e8cb89e7a4c598caa1204820a3ec93
PhungNhat <phungnhatfr2352@gmail.com>
2025-04-02 09:32:50 -0400

    task8

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -31,10 +31,47 @@ regd_users.post("/login", (req,res) => {
     }
 });
 
+//Middleware to verify authentication
+const verifyToken = (req, res, next) => {
+    let token = req.session.token;
+
+    if (!token) {
+        return res.status(404).json({
+            error: "Error: Access denied!"
+        })
+    }
+
+    jwt.verify(token, SECRET_KEY, (error, decoded) => {
+        if (error) {
+            return res.status(404).json({
+                error: "Invalid token"
+            })
+        } else {
+            req.username = decoded.username;
+            next();
+        }
+    })
+}
 // Add a book review
-regd_users.put("/auth/review/:isbn", (req, res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+regd_users.put("/auth/review/:isbn",verifyToken ,(req, res) => {
+  let username = req.username;
+  let isbn = req.params.isbn;
+  let {review} = req.body;
+
+  if (!books[isbn]) {
+    return res.status(401).json({error: "Can't find this book"});
+  }
+
+  if (!review) {
+    return res.status(202).json({message: "You don't comment"});
+  }
+
+  books[isbn].review[username] = review;
+
+  res.send(JSON.stringify({
+    message : books[isbn].review[username]
+  }, null, 2));
+
 });
 
 module.exports.authenticated = regd_users;
