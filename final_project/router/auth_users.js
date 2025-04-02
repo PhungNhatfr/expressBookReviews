ace7f652f244c8d50cf6177debabe09d3e10f2ea
PhungNhat <phungnhatfr2352@gmail.com>
2025-04-02 08:22:49 -0400

    Complete the code for logging in

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -4,19 +4,31 @@ let books = require("./booksdb.js");
 const regd_users = express.Router();
 
 let users = [];
+const SECRET_KEY = "12345";
 
 const isValid = (username)=>{ 
-    return users.find(user => user.username = username);
+    return users.find(user => user.username === username);
 }
 
-const authenticatedUser = (username,password)=>{ //returns boolean
-//write code to check if username and password match the one we have in records.
+const authenticatedUser = (username,password)=>{ 
+    return users.find(user => user.username && user.username === username 
+                            && user.password && user.password === password);
 }
 
 //only registered users can login
 regd_users.post("/login", (req,res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    let {username, password} = req.body;
+
+    if (authenticatedUser(username, password)) {
+        let token = jwt.sign({username}, SECRET_KEY, {expiresIn: "1h"});
+        res.send(JSON.stringify({
+            message: "Login successful",
+            token
+        }, null, 2));
+        req.session.token = token;
+    } else {
+        return res.status(404).json({error: "The user doesn't exist"});
+    }
 });
 
 // Add a book review
